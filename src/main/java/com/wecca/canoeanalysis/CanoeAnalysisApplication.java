614c183b883c749eadc5dbc65557e0937137ba41
Tyler-L-UWO <tliquorn@uwo.ca>
2024-05-24 12:51:11 -0400

    Added JFoenix for improved controls and styling, beggining work on UI redesign

--- src/main/java/com/wecca/canoeanalysis/CanoeAnalysisApplication.java
+++ src/main/java/com/wecca/canoeanalysis/CanoeAnalysisApplication.java
@@ -12,17 +12,17 @@ public class CanoeAnalysisApplication extends Application {
     // The entry point of the application
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(CanoeAnalysisApplication.class.getResource("canoe-analysis-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(CanoeAnalysisApplication.class.getResource("view/canoe-analysis-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 625);
         stage.setTitle("PADDL");
         stage.setScene(scene);
         stage.show();
 
         // Add the CSS file to the scene's stylesheets
-        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        scene.getStylesheets().add(getClass().getResource("css/style.css").toExternalForm());
 
         // Adding Logo Icon
-        Image icon = new Image("file:src/main/resources/com/wecca/canoeanalysis/canoe.png");
+        Image icon = new Image("file:src/main/resources/com/wecca/canoeanalysis/images/canoe.png");
         stage.getIcons().add(icon);
     }
 
