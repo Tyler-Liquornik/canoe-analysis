ee5f7b362fc35dff8c56a1df28b246b2ec6b41f2
Tyler-L-UWO <tyler.liquornik@gmail.com>
2024-06-10 21:41:10 -0400

    Interim commit

---
+++ src/main/java/com/wecca/canoeanalysis/services/CssManagerService.java
@@ -0,0 +1,8 @@
+package com.wecca.canoeanalysis.services;
+
+/**
+ * This service will dynamically update PADDL CSS files
+ * This will allow a flexible approach to allow CSS changes to persist
+ */
+public class CssManagerService {
+}
