542e3e2e2ecd81826ce59c86061d6336ff3dd842
Tyler Liquornik <tliquornik@ellisdon.com>
2024-06-27 01:29:58 -0400

    load tree highlight load, delete, clear all restored

--- src/main/java/com/wecca/canoeanalysis/components/controls/LoadTreeItem.java
+++ src/main/java/com/wecca/canoeanalysis/components/controls/LoadTreeItem.java
@@ -1,6 +1,10 @@
 package com.wecca.canoeanalysis.components.controls;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.wecca.canoeanalysis.models.DiscreteLoadDistribution;
 import com.wecca.canoeanalysis.models.Load;
+import com.wecca.canoeanalysis.models.PointLoad;
+import com.wecca.canoeanalysis.models.UniformlyDistributedLoad;
 import javafx.scene.control.TreeItem;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,62 +13,89 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+@Getter @Setter
 public class LoadTreeItem extends TreeItem<String> {
-    @Getter @Setter
-    private int loadId;
-    @Getter @Setter
-    private int nestedLoadId;
-    private final List<LoadTreeItem> children;
+    private int loadId; // base load (pLoad, non-nested dLoad, loadDist)
+    private int loadTypeId; // within type
+    private int nestedLoadId; // within nesting of another load (loadDist)
+    private int fieldId; // within fields of a load
+    private LoadTreeItem parentLoadItems;
+    private List<LoadTreeItem> childrenLoadItems;
 
     // Root node
     public LoadTreeItem() {
         super("");
         this.loadId = -1;
         this.nestedLoadId = -1;
-        this.children = new ArrayList<>();
+        this.fieldId = -1;
+        this.childrenLoadItems = new ArrayList<>();
     }
 
     // Non-nested load field
-    public LoadTreeItem(int loadId, String value) {
+    public LoadTreeItem(int loadId, int fieldId, String value) {
         super(value);
         this.loadId = loadId;
+        this.loadTypeId = -1;
         this.nestedLoadId = -1;
-        this.children = new ArrayList<>();
+        this.fieldId = fieldId;
+        this.childrenLoadItems = new ArrayList<>();
     }
 
     // Nested load field
-    public LoadTreeItem(int loadId, int nestedLoadId, String value) {
+    public LoadTreeItem(int loadId, int nestedLoadId, int fieldId, String value) {
         super(value);
         this.loadId = loadId;
+        this.loadTypeId = -1;
         this.nestedLoadId = nestedLoadId;
-        this.children = new ArrayList<>();
+        this.fieldId = fieldId;
+        this.childrenLoadItems = new ArrayList<>();
     }
 
     // Non-nested load (pLoad, dLoad, loadDist)
-    public LoadTreeItem(int loadId, Load load) {
-        super(load.getType());
+    public LoadTreeItem(int loadId, int loadTypeId, Load load) {
+        super(String.format("%s: %c_%d", load.getType(), getTypeChar(load), (loadTypeId + 1)));
         this.loadId = loadId;
+        this.loadTypeId = loadTypeId;
         this.nestedLoadId = -1;
-        this.children = new ArrayList<>();
+        this.fieldId = -1;
+        this.childrenLoadItems = new ArrayList<>();
     }
 
     // Nested load (dLoad child of loadDist)
-    public LoadTreeItem(int loadId, int nestedLoadId, Load load) {
-        super(load.getType());
+    public LoadTreeItem(int loadId, int nestedLoadId, int loadTypeId, Load load) {
+        super(String.format("%s: %c_%s", load.getType(), getTypeChar(load), (loadTypeId + 1)));
         this.loadId = loadId;
+        this.loadTypeId = loadTypeId;
         this.nestedLoadId = nestedLoadId;
-        this.children = new ArrayList<>();
+        this.fieldId = -1;
+        this.childrenLoadItems = new ArrayList<>();
     }
 
-    // Method to add child to the overridden children list and ensure children stay sorted
+    @JsonIgnore
+    private static char getTypeChar(Load load) {
+        switch (load) {
+            case PointLoad ignoredLoad -> {return 'p';}
+            case UniformlyDistributedLoad ignoredLoad -> {return 'w';}
+            case DiscreteLoadDistribution ignoredLoad -> {return 'd';}
+            default -> throw new IllegalArgumentException("Provide a valid load");
+        }
+    }
+
+    /**
+     * @param child the child to add, need to ensure that the child's indices are correct
+     */
     public void addChild(LoadTreeItem child) {
-        children.add(child);
-        for (int i = 0; i < children.size(); i++) {children.get(i).setLoadId(i);}
-        children.sort(Comparator.comparingInt(LoadTreeItem::getLoadId));
-        getChildren().setAll(children);
+        child.setParentLoadItems(this);
+        childrenLoadItems.add(child);
+        childrenLoadItems.sort(Comparator.comparingInt(LoadTreeItem::getLoadId));
+        getChildren().setAll(childrenLoadItems);
     }
 
     public boolean isNested() {
         return nestedLoadId > -1;
+    }
+
+    public boolean isField() {
+        return  fieldId > -1;
     }
 }
