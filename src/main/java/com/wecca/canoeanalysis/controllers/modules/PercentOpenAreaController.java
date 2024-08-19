b3a7f1f2f7e5779f547b4729c7b7d3d50520c351
Jaredsegal1 <jsegal28@uwo.ca>
2024-08-19 19:57:57 -0400

    Jared Percent open area first commit

--- src/main/java/com/wecca/canoeanalysis/controllers/modules/PercentOpenAreaController.java
+++ src/main/java/com/wecca/canoeanalysis/controllers/modules/PercentOpenAreaController.java
@@ -2,9 +2,24 @@ package com.wecca.canoeanalysis.controllers.modules;
 
 import com.wecca.canoeanalysis.CanoeAnalysisApplication;
 import com.wecca.canoeanalysis.controllers.MainController;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import lombok.Setter;
+import ij.ImagePlus;
+import ij.IJ;
+import ij.plugin.filter.Analyzer;
+import ij.process.ImageProcessor;
+import ij.measure.ResultsTable;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -13,11 +28,87 @@ public class PercentOpenAreaController implements Initializable {
     @Setter
     private static MainController mainController;
 
+    @FXML
+    private ImageView imageview;
+    @FXML
+    private Label OpenArea, TotalArea, PercentOpenArea;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         // Set the local instance of the main controller
         setMainController(CanoeAnalysisApplication.getMainController());
 
         mainController.resetToolBarButtons();
+    }
+
+
+    public void uploadButton() {
+
+        Stage popupStage = new Stage();
+
+        // Create a FileChooser to select image files
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Image File");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg", "*.gif")
+        );
+        File file = fileChooser.showOpenDialog(popupStage);
+        if (file != null) {
+            try {
+                // Load the selected image file
+                Image image = new Image(new FileInputStream(file));
+
+                // Display the image in the main ImageView
+                imageview.setImage(image);
+                // Close the popup window
+                popupStage.close();
+            } catch (FileNotFoundException ex) {
+                ex.printStackTrace();
+            }
+
+            ImagePlus img = IJ.openImage(file.getAbsolutePath());
+
+            // Convert to grayscale if needed
+            ImageProcessor ip = img.getProcessor();
+            if (ip.getBitDepth() != 8) {
+                IJ.run(img, "8-bit", "");
+            }
+
+            IJ.run(img, "Subtract Background...", "rolling=50");
+            IJ.run(img, "Gaussian Blur...", "sigma=2");
+
+            // Threshold the image to segment the mesh and open areas
+            IJ.setAutoThreshold(img, "Default");
+            IJ.run(img, "Convert to Mask", "");
+
+            // Analyze particles to get the area of open spaces
+            IJ.run(img, "Set Measurements...", "area redirect=None decimal=3");
+            IJ.run(img, "Analyze Particles...", "clear");
+
+            // Get the ResultsTable that contains the measurements
+            ResultsTable rt = Analyzer.getResultsTable();
+
+            // Calculate the total area of the open spaces
+            double openArea = 0.0;
+            for (int i = 0; i < rt.getCounter(); i++) {
+                openArea += rt.getValue("Area", i);
+            }
+
+            // Get the total area of the image
+            double totalArea = img.getWidth() * img.getHeight();
+
+            // Calculate the percent open area
+            double percentOpenArea = (openArea / totalArea) * 100;
+
+            // Set the results
+            OpenArea.setText(String.valueOf(openArea));
+            TotalArea.setText(String.valueOf(totalArea));
+            PercentOpenArea.setText(String.format("%.2f%%", percentOpenArea));
+            System.out.printf("Percent Open Area: %.2f", percentOpenArea);
+        }
+    }
+
+    public void clearImage() {
+        imageview.setImage(null);
     }
 }
