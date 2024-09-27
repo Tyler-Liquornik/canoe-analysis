8d45c27fd1f2e94a29480e7bacfdb6a4ad3ea1cc
Tyler-L-UWO <tyler.liquornik@gmail.com>
2024-09-27 16:40:27 -0400

    tyler's first testing commit, first software meeting

--- src/main/java/com/wecca/canoeanalysis/controllers/modules/BeamController.java
+++ src/main/java/com/wecca/canoeanalysis/controllers/modules/BeamController.java
@@ -33,6 +33,7 @@ import java.util.function.Consumer;
  * Primary controller for longitudinal analysis of a beam
  */
 @Traceable
+// Tyler Liquornik
 public class BeamController implements Initializable {
     @FXML
     private Label axisLabelR, lengthLabel, pointDirectionLabel, pointMagnitudeLabel, pointLocationLabel,
