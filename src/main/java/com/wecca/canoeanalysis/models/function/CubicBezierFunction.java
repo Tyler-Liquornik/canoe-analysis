91f24c467b97a18851ddc8d86154c0eb5e30b3d6
drayner9 <drayner9@uwo.ca>
2025-01-07 22:15:52 -0500

    Added Json Ignore to control points to fix bug

--- src/main/java/com/wecca/canoeanalysis/models/function/CubicBezierFunction.java
+++ src/main/java/com/wecca/canoeanalysis/models/function/CubicBezierFunction.java
@@ -197,6 +197,7 @@ public class CubicBezierFunction implements ParameterizedBoundedUnivariateFuncti
     /**
      * @return a list of Point2D objects for each of the control points
      */
+    @JsonIgnore
     public List<Point2D> getControlPoints() {
         Point2D p1 = new Point2D(controlX1, controlY1);
         Point2D p2 = new Point2D(controlX2, controlY2);
