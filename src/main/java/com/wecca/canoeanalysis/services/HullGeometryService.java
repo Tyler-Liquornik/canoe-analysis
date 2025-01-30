20333676a1a277e53e2a85474c9e3cdb6d5a24cc
Tyler-L-UWO <tyler.liquornik@gmail.com>
2025-01-29 20:51:04 -0500

    bezier top view implemented

--- src/main/java/com/wecca/canoeanalysis/services/HullGeometryService.java
+++ src/main/java/com/wecca/canoeanalysis/services/HullGeometryService.java
@@ -1,7 +1,6 @@
 package com.wecca.canoeanalysis.services;
 
 import com.wecca.canoeanalysis.aop.Traceable;
-import com.wecca.canoeanalysis.controllers.MainController;
 import com.wecca.canoeanalysis.controllers.modules.HullBuilderController;
 import com.wecca.canoeanalysis.models.canoe.Hull;
 import com.wecca.canoeanalysis.models.canoe.HullSection;
@@ -11,7 +10,6 @@ import com.wecca.canoeanalysis.models.function.Section;
 import com.wecca.canoeanalysis.utils.CalculusUtils;
 import javafx.geometry.Point2D;
 import javafx.scene.shape.Rectangle;
-import lombok.NonNull;
 import lombok.Setter;
 import java.util.ArrayList;
 import java.util.Comparator;
